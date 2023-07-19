6b1da98ec220c9b48e2bfbfe95b11d7690299c16
Daniel Vecchio Lopez <vecchiolopez@gmail.com>
2023-07-19 22:20:27 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'VLC Kasm Registry',
+    description: 'Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://dvelop.github.io//kasm-registry/',
+    contactUrl: 'https://github.com/dvelop/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
